8d8a72dcc226332ed235e825cbd3b49f0ae6d715
Fabiano Silva <fabianors.silva@gmail.com>
2026-05-31 08:55:23 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,6 +3,6 @@
 // - Project URL
 // - anon public key
 window.BOLAO_SUPABASE_CONFIG = {
-  url: "COLE_AQUI_A_PROJECT_URL",
-  anonKey: "COLE_AQUI_A_ANON_PUBLIC_KEY"
+  url: "https://zmnwjzzefwmvtflpfkoi.supabase.co",
+  anonKey: "sb_publishable_aWahlisIb8OUmRNJoERagA_9MmOC0al"
 };
